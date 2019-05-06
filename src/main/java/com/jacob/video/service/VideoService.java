8fb86e192d16de0b6f34d1e63970bc7167da0817
JacobLei <17763636521@163.com>
2019-05-06 21:02:24 +0800

    添加VideoService和VideoImpl

--- src/main/java/com/jacob/video/service/VideoService.java
+++ src/main/java/com/jacob/video/service/VideoService.java
@@ -1,4 +1,17 @@
 package com.jacob.video.service;
 
+import com.jacob.video.domain.Video;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 视频业务类接口
+ */
 public interface VideoService {
+    List<Video> findAll();
+    Video findById(int id);
+    int update(Video video);
+    int delete(int id);
+    int save(Video video);
 }
--- src/main/java/com/jacob/video/service/impl/VideoImpl.java
+++ src/main/java/com/jacob/video/service/impl/VideoImpl.java
@@ -1,4 +1,40 @@
 package com.jacob.video.service.impl;
 
-public class VideoImpl {
+import com.jacob.video.domain.Video;
+import com.jacob.video.mapper.VideoMapper;
+import com.jacob.video.service.VideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class VideoImpl implements VideoService {
+
+    @Autowired
+    private VideoMapper videoMapper;
+
+    @Override
+    public List<Video> findAll() {
+        return videoMapper.findAll();
+    }
+
+    @Override
+    public Video findById(int id) {
+        return videoMapper.findById(id);
+    }
+
+    @Override
+    public int update(Video video) {
+        return videoMapper.update(video);
+    }
+
+    @Override
+    public int delete(int id) {
+        return videoMapper.delete(id);
+    }
+
+    @Override
+    public int save(Video video) {
+        return videoMapper.save(video);
+    }
 }
