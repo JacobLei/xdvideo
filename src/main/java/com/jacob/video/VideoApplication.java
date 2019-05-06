59499c8157ca15440588768745cf9b7d43a96419
JacobLei <17763636521@163.com>
2019-05-06 22:26:56 +0800

    添加MapperScan

--- src/main/java/com/jacob/video/VideoApplication.java
+++ src/main/java/com/jacob/video/VideoApplication.java
@@ -1,13 +1,14 @@
 package com.jacob.video;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.jacob.video.mapper")
 public class VideoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VideoApplication.class, args);
 	}
-
 }
