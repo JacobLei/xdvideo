b0a7d2df04f4e33324efd5f9d63a886e8dffed70
JacobLei <17763636521@163.com>
2019-05-06 15:12:23 +0800

    添加WeChatConfig配置类

---
+++ src/main/java/com/jacob/video/config/WeChatConfig.java
@@ -0,0 +1,40 @@
+package com.jacob.video.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * 微信配置类
+ */
+@Configuration
+@PropertySource(value="classpath:application.properties")
+public class WeChatConfig {
+    /**
+     * 公众号id
+     */
+    @Value("${wxpay.appid}")
+    private String appId;
+
+    /**
+     * 公众号密钥
+     */
+    @Value("${wxpay.appsecret}")
+    private String appsSecret;
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getAppsSecret() {
+        return appsSecret;
+    }
+
+    public void setAppsSecret(String appsSecret) {
+        this.appsSecret = appsSecret;
+    }
+}
